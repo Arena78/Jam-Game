45544b72281b3725178667e3e397a932b5b3e364
Arena78 <103265035+Arena78@users.noreply.github.com>
2026-07-22 23:16:47 +0200

    fixed an enemy bug and added resources

--- scenes/globals/player_data.gd
+++ scenes/globals/player_data.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var currentEquipment := [];
+var currentEquipment := ["Sword"];
 var health := 1.0;
 var level := 1;
 var money := 0.0;
---
+++ scenes/prefabs/items/skill.gd
@@ -0,0 +1,14 @@
+class_name Skill
+extends Resource
+
+@export var texture: Texture2D;
+
+@export var name: String = "";
+
+@export var autoTrigger := false;
+@export var cast_time := 1.0;
+@export var cooldown_time := 1.0;
+
+@export var damage := 1.0;
+@export var range := 1.0;
+@export var homing := false;
